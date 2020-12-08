6c937572a49c78dcbfe427e35b6dccbc4dc3923c
Blarc <jakob.malezic@gmail.com>
2020-12-08 16:35:40 +0100

    init commit

---
+++ src/main/java/rso/usercatalogue/UserCatalogueApplication.java
@@ -0,0 +1,13 @@
+package rso.usercatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rso/usercatalogue/UserCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package rso.usercatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
