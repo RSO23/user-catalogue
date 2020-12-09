5287cfddce1829fb242e319829c52801d7264bda
Blarc <jakob.malezic@gmail.com>
2020-12-09 11:01:42 +0100

    added import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO _user (email, name, password, username) VALUES ('tester1@gmail.com', 'tester1', 'tester1', 'tester1');
+INSERT INTO _user (email, name, password, username) VALUES ('tester2@gmail.com', 'tester2', 'tester2', 'tester2');
+INSERT INTO _user (email, name, password, username) VALUES ('tester3@gmail.com', 'tester3', 'tester3', 'tester3');
