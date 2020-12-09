9bea53e4ccd0f170e86f1b0cd32754f3d6d99e4e
Blarc <jakob.malezic@gmail.com>
2020-12-09 15:16:05 +0100

    fixed imports and workflow

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO _user (email, name, password, username) VALUES ('tester1@gmail.com', 'tester1', 'tester1', 'tester1');
-INSERT INTO _user (email, name, password, username) VALUES ('tester2@gmail.com', 'tester2', 'tester2', 'tester2');
-INSERT INTO _user (email, name, password, username) VALUES ('tester3@gmail.com', 'tester3', 'tester3', 'tester3');
+INSERT INTO public._user (id, email, name, password, username) VALUES (4, 'test1@gmail.com', 'test1', '$2a$10$SbOhSDfGjJVeBY5XF/NpRu6JQQi95UNOhOWWQDA3bJo86tz2w7Shy', 'test1');
+INSERT INTO public._user (id, email, name, password, username) VALUES (5, 'test2@gmail.com', 'test2', '$2a$10$sCb/f.bGkZCFXVEsiDtrt.7bVcr9GycZEXdSWsqMSjS3GK2E1CT4e', 'test2');
+INSERT INTO public._user (id, email, name, password, username) VALUES (6, 'test3@gmail.com', 'test3', '$2a$10$oHx7hZqQ5rBikaYvjaNK6OpLsBxiePPGutRHvr7uUGeT9keCQmSEq', 'test3');
